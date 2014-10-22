533b647ffb9e759486cd432b6ae1534041b1b92c
Sebastian Herrlinger <hasta84@gmail.com>
2014-10-22 11:05:46 +0200

    More modules, inherit some of node.js

--- tools/prebuild.sh
+++ tools/prebuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python ../tools/js2c.py ../src/js_natives.h ../lib/cinder.js ../lib/path.js
+python ../tools/js2c.py ../src/js_natives.h ../lib/cinder.js ../lib/path.js ../lib/util.js ../lib/module.js
