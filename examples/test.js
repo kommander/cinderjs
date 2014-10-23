1d2b9e3c779f605bceca1f6aa1ac31200082b3e4
kommander <sebastian@formzoo.com>
2014-10-24 00:20:04 +0200

    setInterval and clearTimeout/Interval

--- examples/test.js
+++ examples/test.js
@@ -2,13 +2,15 @@ var timers = require('timers');
 
 log("running test...");
 
+// Oh, how I wish I had a testsuite running already...
+
 // Run a lot of timeouts at the same time
 //for(var i = 0; i < 100; i++){
 //  timers.setTimeout(function(now){
 //    log("Timed out in " + (Date.now() - now) + "ms");
 //  }, 2000, Date.now());
 //}
-
+//
 //function doTimeout(){
 //  timers.setTimeout(function(now){
 //    log("Timed out in " + (Date.now() - now) + "ms");
@@ -16,17 +18,26 @@ log("running test...");
 //  }, 100, Date.now());
 //}
 //doTimeout();
-
+//
 // Test clearing a timer
-var timer = timers.setTimeout(function(){
-  log('Error: We should not get here, as the timeout was canceled');
-}, 900);
+//var timer1 = timers.setTimeout(function(){
+//  log('Error: We should not get here, as the timeout was canceled');
+//}, 900);
+//
+//timers.setTimeout(function(){
+//  log('Fine, this one wasn not cleared');
+//}, 1100);
+//
+//timers.setTimeout(function(){
+//  log('Clearing timer...');
+//  timers.clearTimeout(timer1);
+//}, 500);
 
+// Test interval
+var timer = timers.setInterval(function(now){
+  log("Time passed: " + (Date.now() - now) + "ms");
+}, 100, Date.now());
 timers.setTimeout(function(){
-  log('Fine, this one wasn not cleared');
-}, 1100);
-
-timers.setTimeout(function(){
-  log('Clearing timer...');
-  timers.clearTimeout(timer);
+  log('Clearing interval...');
+  timers.clearInterval(timer);
 }, 500);
