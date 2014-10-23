fe98cd10fb41c405a7ea896255f66b4fcba2841e
kommander <sebastian@formzoo.com>
2014-10-23 14:54:27 +0200

    First working setTimeout with seconds resolution

--- examples/test.js
+++ examples/test.js
@@ -1,26 +1,9 @@
 log("External World!" + Math.random());
 
-var line = [{
-  x: 0,
-  y: 0
-},
-{
-  x: 400,
-  y: 400
-}];
-
-var clear = function(){
-  log("clear");
-}
-
 __draw__(function(){
-  clear();
-  //log("Drawing from JS...");
-  //glDrawLine(line[0], line[1]);
-  glDrawSolidCircle(line[1], 30);
+  
 });
 
-// TODO: implement setTimeout
-//setTimeout(function(){
-//  log("Timed out...");
-//}, 2000);
+setTimeout(function(){
+  log("Timed out...");
+}, 2000);
