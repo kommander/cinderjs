3838acf8062b327593a370523d44860a2632d65c
Sebastian Herrlinger <hasta84@gmail.com>
2014-10-24 03:02:55 +0200

    have better timer accuracy

--- examples/test.js
+++ examples/test.js
@@ -5,12 +5,12 @@ log("running test...");
 // Oh, how I wish I had a testsuite running already...
 
 // Run a lot of timeouts at the same time
-//for(var i = 0; i < 100; i++){
-//  timers.setTimeout(function(now){
-//    log("Timed out in " + (Date.now() - now) + "ms");
-//  }, 2000, Date.now());
-//}
-//
+for(var i = 0; i < 100; i++){
+  timers.setTimeout(function(now){
+    log("Timed out in " + (Date.now() - now) + "ms");
+  }, 2000, Date.now());
+}
+
 //function doTimeout(){
 //  timers.setTimeout(function(now){
 //    log("Timed out in " + (Date.now() - now) + "ms");
@@ -20,18 +20,18 @@ log("running test...");
 //doTimeout();
 //
 // Test clearing a timer
-//var timer1 = timers.setTimeout(function(){
-//  log('Error: We should not get here, as the timeout was canceled');
-//}, 900);
-//
-//timers.setTimeout(function(){
-//  log('Fine, this one wasn not cleared');
-//}, 1100);
-//
-//timers.setTimeout(function(){
-//  log('Clearing timer...');
-//  timers.clearTimeout(timer1);
-//}, 500);
+var timer1 = timers.setTimeout(function(){
+  log('Error: We should not get here, as the timeout was canceled');
+}, 900);
+
+timers.setTimeout(function(){
+  log('Fine, this one wasn not cleared');
+}, 1100);
+
+timers.setTimeout(function(){
+  log('Clearing timer...');
+  timers.clearTimeout(timer1);
+}, 500);
 
 // Test interval
 var timer = timers.setInterval(function(now){
@@ -40,4 +40,4 @@ var timer = timers.setInterval(function(now){
 timers.setTimeout(function(){
   log('Clearing interval...');
   timers.clearInterval(timer);
-}, 500);
+}, 450);
