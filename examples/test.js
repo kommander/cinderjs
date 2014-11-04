d82ebac0753d334202848546e963be040b269224
Sebastian Herrlinger <sebastian@formzoo.com>
2014-11-04 06:55:00 +0100

    Have global timer methods

--- examples/test.js
+++ examples/test.js
@@ -1,18 +1,16 @@
-var timers = require('timers');
-
 console.log("running test...");
 
 // Oh, how I wish I had a testsuite running already...
 
 // Run a lot of timeouts at the same time
 for(var i = 0; i < 100; i++){
-  timers.setTimeout(function(now){
+  setTimeout(function(now){
     console.log("Timed out in " + (Date.now() - now) + "ms");
   }, 2000, Date.now());
 }
 
 //function doTimeout(){
-//  timers.setTimeout(function(now){
+//  setTimeout(function(now){
 //    console.log("Timed out in " + (Date.now() - now) + "ms");
 //    doTimeout();
 //  }, 100, Date.now());
@@ -20,24 +18,24 @@ for(var i = 0; i < 100; i++){
 //doTimeout();
 //
 // Test clearing a timer
-var timer1 = timers.setTimeout(function(){
+var timer1 = setTimeout(function(){
   console.log('Error: We should not get here, as the timeout was canceled');
 }, 900);
 
-timers.setTimeout(function(){
-  console.log('Fine, this one wasn not cleared');
+setTimeout(function(){
+  console.log('Fine, this one was not cleared');
 }, 1100);
 
-timers.setTimeout(function(){
+setTimeout(function(){
   console.log('Clearing timer...');
-  timers.clearTimeout(timer1);
+  clearTimeout(timer1);
 }, 500);
 
 // Test interval
-var timer = timers.setInterval(function(now){
+var timer = setInterval(function(now){
   console.log("Time passed: " + (Date.now() - now) + "ms");
 }, 100, Date.now());
-timers.setTimeout(function(){
+setTimeout(function(){
   console.log('Clearing interval...');
-  timers.clearInterval(timer);
+  clearInterval(timer);
 }, 450);
