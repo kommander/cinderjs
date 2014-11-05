09c3f5b94fd34bd1746a1145221b3c4e6faae6f9
Sebastian Herrlinger <sebastian@formzoo.com>
2014-11-05 10:43:55 +0100

    Adjust example to glNext (shaders still broken)

--- examples/cubes.js
+++ examples/cubes.js
@@ -1,8 +1,11 @@
 //
 // Cubes Example
-var Material = require('material');
-var Light = require('light');
 var gl = require('gl');
+var Camera = require('camera');
+var Shader = require('shader');
+var Batch = require('batch');
+
+app.addAssetDirectory(__dirname + '/assets/');
 
 // Screen size holder
 var ctxSize = { x: 640, y: 480 };
@@ -13,69 +16,57 @@ var mouse = { x: 0, y: 0 };
 // Rotation
 var rotation = { x: 0, y: 0, z: 0 };
 
-var currentLight = 0;
-
 var drawMethod = 0;
 var wireframe = false;
-var cullface = false;
 
-// Create example material
-var mat = new Material();
-mat.ambient(.0, .6, .6, 1);
-mat.diffuse(.0, .4, .8, 1);
-mat.specular(.0, .8, .8, 1);
-mat.emission(.0, .2, .3, 1);
-mat.shininess(100);
+var cam = new Camera();
+cam.lookAt( 3, 2, 4, 0, 0, 0 );
+cam.setPerspective( 60.0, app.getAspectRatio(), 1.0, 1000.0 );
 
-var light = new Light();
-light.ambient(0.5, 0.1, 0.5, 1);
+var glslProg = new Shader( 'shader.vert', 'shader.frag' );
+var batch = new Batch(0, glslProg);
 
 // GL Draw loop
 var loop = function(timePassed, mx, my){
+  gl.enableDepthRead();
+  gl.enableDepthWrite();
+
   // Update mouse position
   mouse.x = mx;
   mouse.y = my;
   
-  gl.clear( 0.1, 0.1, 0.1 );
+  gl.pushMatrices();
+  
+  gl.clear( 0.1, 0.1, 0.11 );
   
   // Update rotation
   rotation.y += 0.6;
   rotation.z += 0.8;
   
-  gl.pushMatrices();
-  gl.enableDepthRead();
-  
+  gl.setMatrices(cam.id);
+
   if(wireframe) gl.enableWireframe();
   
-  gl.enable(gl.LIGHTING);
-  light.enable()
+  batch.draw();
 
-  gl.translate(mouse.x, mouse.y);
-  gl.rotate(rotation.x, rotation.y, rotation.z);
-  
-  mat.apply();
-  
-  if(drawMethod == 0) {
-    gl.drawTorus(60, 30, 16, 16);
-  } else if(drawMethod == 1) {
-    gl.drawCube(0, 0, 0, 60, 60, 60);
-  } else if(drawMethod == 2) {
-    gl.drawCylinder(60, 30, 50, 16, 4);
-  } else if(drawMethod == 3) {
-    gl.drawSphere(0, 0, 0, 30, 16);
-  }
-  
-  light.disable();
-  gl.disable(gl.LIGHTING);
-  
   if(wireframe) gl.disableWireframe();
 
-  gl.disableDepthRead();
   gl.popMatrices();
+
+  gl.disableDepthRead();
+  gl.disableDepthWrite();
+
 }
 
 // Register draw loop (executed each frame, allows drawing to window)
 app.draw(loop);
+
+app.on('resize', function(w, h){
+  ctxSize.x = w;
+  ctxSize.y = h;
+
+  cam.setPerspective( 60.0, app.getAspectRatio(), 1.0, 1000.0 );
+});
 
 // Handle key events
 app.on('keydown', function( evt ){
@@ -87,15 +78,6 @@ app.on('keydown', function( evt ){
     toggleV8Stats();
   } else if(evt.charCode == 119) { // W
     wireframe = !wireframe;
-  } else if(evt.charCode == 114) { // R
-    drawMethod++
-    if(drawMethod > 3){
-      drawMethod = 0
-    }
-  } else if(evt.charCode == 108) { // L
-    
-  } else if(evt.charCode == 102) { // F
-    
   }
 
 });
